e04fd1410a30a9fcc57aa931abc91a8f9be115ec
Oskar Reszka <oskarreszka12@gmail.com>
2022-11-02 19:11:43 +0100

    initial

---
+++ MWO/ICarController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWO
+{
+    public interface ICarController
+    {
+        bool CreateCar(Car car);
+        Car GetCar(int carId);
+        bool RemoveCar(int carId);
+        bool UpdateCar(int carId, Car car);
+    }
+}
